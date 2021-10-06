3fc097f0ea4a1b05f50f3c8fec4bd1c73bc56ab2
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-10-06 11:02:20 +0300

    adding bootstrap

--- tribune/settings.py
+++ tribune/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'news',
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'news',
 ]
 
 MIDDLEWARE = [
